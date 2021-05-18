b38730ee031353511e5385fead5738f504fac1d6
turituri08 <t.y.bsk@icloud.com>
2021-05-18 00:27:59 +0000

    課題３です

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
  root to: "homes#top"
- get "homes/about" => "homes#about", as: "about"
+ get "/home/about" => "homes#about", as: "about"
  devise_for :users
 
  resources :books, only:[:index, :show, :edit, :update, :create, :update, :destroy]
